54437f1f3b3791e598309e3fd546d946d3d9be63
mason.owens <mopublic8756@gmail.com>
2021-05-27 22:26:58 +0900

    Update edti.html and Add Admin new

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
 
-import java.sql.Timestamp;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.form.AdminCreateForm;
 import com.example.demo.form.UserCreateForm;
 import com.example.demo.form.UserUpdateForm;
 import com.example.demo.model.SiteUser;
@@ -27,8 +26,10 @@ public class UserController {
 	
 	private final String REDIRECT_SHOW_URL = "redirect:/trip_planner/user_account/show";
 	private final String REDIRECT_LOGIN_URL = "redirect:/trip_planner/login?register";
+	private final String REDIRECT_LIST_URL = "redirect:/trip_planner/user_account/admin/list";
 	private final String LIST_TEMPLATE_PATH = "/trip_planner/user_account/list";
 	private final String RESISTER_TEMPLATE_PATH = "/trip_planner/user_account/register";
+	private final String RESISTER_ADMIN_TEMPLATE_PATH = "/trip_planner/user_account/register_admin";
 	private final String EDIT_TEMPLATE_PATH = "/trip_planner/user_account/edit";
 	private final String USER_TEMPLATE_PATH = "/trip_planner/user_account/user";
 	
@@ -49,7 +50,7 @@ public class UserController {
 	public String register(@ModelAttribute UserCreateForm userCreateForm) {
 		return RESISTER_TEMPLATE_PATH;
 	}
-
+	
 	@PostMapping("create")
 	public String process(@Validated @ModelAttribute UserCreateForm userCreateForm, final BindingResult result) {
 		if (result.hasErrors()) {
@@ -63,6 +64,26 @@ public class UserController {
 		service.save(user);
 		return REDIRECT_LOGIN_URL;
 	}
+	
+	@GetMapping("register_admin")
+	public String registerAdmin(@ModelAttribute AdminCreateForm adminCreateForm) {
+		return RESISTER_ADMIN_TEMPLATE_PATH;
+	}
+	
+	@PostMapping("create_admin")
+	public String process(@Validated @ModelAttribute AdminCreateForm adminCreateForm, final BindingResult result) {
+		if (result.hasErrors()) {
+			return RESISTER_ADMIN_TEMPLATE_PATH;
+		}
+
+		// アカウントを有効にする
+		adminCreateForm.setActive(true);
+
+		SiteUser user = adminCreateForm.toEntity();
+		service.save(user);
+		return REDIRECT_LIST_URL;
+	}
+	
 	@GetMapping("edit/{username}")
 	public String edit(@PathVariable String username, Model model) {
 		SiteUser user = service.findOne(username);
@@ -75,23 +96,13 @@ public class UserController {
 		if(result.hasErrors()) {
 			return EDIT_TEMPLATE_PATH;
 		}
-
 		SiteUser user = service.findOne(userUpdateForm.getUsername());
-
-		Timestamp created_at = user.getCreated_at();
-		String role = user.getRole();
-		boolean isAdmin = user.isAdmin();
-		boolean isActive = user.isActive();
-
-		userUpdateForm.setCreated_at(created_at);
-		userUpdateForm.setRole(role);
-		userUpdateForm.setAdmin(isAdmin);
-		userUpdateForm.setActive(isActive);
+		userUpdateForm.setCreated_at(user.getCreated_at());
+		userUpdateForm.setRole(user.getRole());
+		userUpdateForm.setAdmin(user.isAdmin());
+		userUpdateForm.setActive(user.isActive());
 		SiteUser updatedUser = userUpdateForm.toEntity();
-
 		service.save(updatedUser);
-
-		model.addAttribute("user", user);
 		return REDIRECT_SHOW_URL +"/" + userUpdateForm.getUsername();
 	}
 }
---
+++ src/main/java/com/example/demo/form/AdminCreateForm.java
@@ -0,0 +1,80 @@
+package com.example.demo.form;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.example.demo.annotation.CustomCheck;
+import com.example.demo.model.SiteUser;
+import com.example.demo.util.Role;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminCreateForm implements Serializable {
+	
+	/**　シリアルバージョンUID. */
+	private static final long serialVersionUID = 1L;
+
+
+	/**　正規表現(半角英数字). */
+	private static final String ALPHANUMERIC_REGEXP = "[a-zA-Z0-9.]*";
+
+	/**　正規表現(半角英数字)のエラーメッセージ. */
+	private static final String ALPHANUMERIC_MESSAGE = "半角英字、数字、ピリオドを使用できます";
+
+	@NotBlank
+    @Size(max=30)
+	@Pattern(regexp = ALPHANUMERIC_REGEXP, message = ALPHANUMERIC_MESSAGE)
+    @CustomCheck(uniqueUsername = "username", message = "既に登録されています")
+    private String username;
+
+
+    @NotBlank
+    @Size(max=255)
+	@Pattern(regexp = ALPHANUMERIC_REGEXP, message = ALPHANUMERIC_MESSAGE)
+    private String password;
+    
+    @NotBlank
+    @Email
+    private String email;
+    private String role;
+    private boolean isAdmin;
+    private boolean isActive;
+    
+    /**
+     * Formクラスの設定内容を文字列で出力する.
+     */
+    @Override
+    public String toString() {
+        return "SiteUser(username: " + this.getUsername() + ", password: " + this.getPassword()
+        +", email: " + this.getEmail() +", role: " + Role.ADMIN.toString() 
+        + ", isAdmin: " + this.isAdmin() +", isActive: " + this.isActive() +")";
+    }
+
+    /**
+     * Formの設定内容をUser Entityクラスに変換する.
+     *
+      * @return ユーザー情報(Entityクラス)
+     */
+    public SiteUser toEntity(){
+
+        SiteUser user = new SiteUser();
+        user.setUsername(this.getUsername());
+        user.setEmail(this.getEmail());
+        user.setPassword(this.getPassword());
+        Timestamp current_time = new Timestamp(System.currentTimeMillis());
+        user.setCreated_at(current_time);
+        user.setUpdated_at(current_time);
+        user.setRole(Role.ADMIN.toString());
+        user.setAdmin(true);
+        user.setActive(this.isActive());
+        return user;
+    }
+}
