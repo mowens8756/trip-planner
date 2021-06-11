cc9ed597c0f24ce6a06a73fb6fb4bf94bd18f6e3
mason.owens <mopublic8756@gmail.com>
2021-06-12 08:14:43 +0900

    Add New and Create Trip

--- src/main/java/com/example/demo/controller/TripController.java
+++ src/main/java/com/example/demo/controller/TripController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,6 +37,7 @@ public class TripController {
 	HttpSession session;
 	
 	private final String NEW_TRIP_TEMPLATE_PATH = "/trip_planner/trip_plan/new_trip";
+	private final String HOME_URL = "/trip_planner/home";
 	private final String REDIRECT_HOME_URL = "redirect:/trip_planner/home";
 	
 	@GetMapping("new_trip")
@@ -55,22 +57,36 @@ public class TripController {
 			return NEW_TRIP_TEMPLATE_PATH;
 		}
 		Trip trip = new Trip();
-		Itinerary itinerary = new Itinerary();
 		Timestamp currentTime = new Timestamp(System.currentTimeMillis());
 		trip.setCreated_at(currentTime);
 		trip.setUpdated_at(currentTime);
-		itinerary.setCreated_at(currentTime);
-		itinerary.setUpdated_at(currentTime);
 		trip.setUsername(tripCreateForm.getUsername());
 		trip.setTitle(tripCreateForm.getTitle());
 		trip.setDestination(tripCreateForm.getDestination());
 		trip.setTravel_days(tripCreateForm.getTravel_days());
 		trip.setCurrency(tripCreateForm.getCurrency());
-		tripService.save(trip);
-		itinerary.setTrip_id(trip.getTrip_id());
-		itinerary.setUsername(tripCreateForm.getUsername());
-		
-		itineraryService.save(itinerary);
+		Trip trip2 =  tripService.save(trip);
+		List<ItineraryCreateForm> itineraryCreateForm = tripCreateForm.getItineraryCreateForm();
+		for (ItineraryCreateForm itineraryForm : itineraryCreateForm) { 
+			Itinerary itinerary = new Itinerary();
+			Timestamp currentTime2 = new Timestamp(System.currentTimeMillis());
+			itinerary.setCreated_at(currentTime2);
+			itinerary.setUpdated_at(currentTime2);
+			itinerary.setTrip_id(trip2.getTrip_id());
+			itinerary.setUsername(tripCreateForm.getUsername());
+			itinerary.setItinerary_date(itineraryForm.getItinerary_date());
+			itinerary.setStart_at(itineraryForm.getStart_at());
+			itinerary.setEnd_at(itineraryForm.getEnd_at());
+			itinerary.setLocation(itineraryForm.getLocation());
+			itinerary.setNote(itineraryForm.getNote());
+			itinerary.setAmount(itineraryForm.getAmount());
+			itineraryService.save(itinerary);
+		}
 		return REDIRECT_HOME_URL;
+	}
+	@GetMapping("home")
+	public String showTripList(Model model) {
+		model.addAttribute("trips", tripService.findAll());
+		return HOME_URL;
 	}
 }
